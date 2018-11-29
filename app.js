48be3008e8d9ce006145f25f93320b0a863e4f7e
Alvin Kwong <alvindkwong@gmail.com>
2018-11-29 02:34:57 +0000

    add edit, update, and destroy

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let bodyParser  = require("body-parser"),
+methodOverride  = require("method-override"),
 mongoose        = require("mongoose"),
 express         = require("express"),
 app             = express()
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 // MONGOOSE/ MODEL CONFIG
@@ -49,7 +51,37 @@ app.post("/blogs", function(req, res) {
   })
 })
 
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) res.redirect("/blogs");
+    else res.render("show", {blog:foundBlog})
+  })
+})
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) res.redirect("/blogs");
+    else res.render("edit", {blog:foundBlog})
+  })
+})
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+    if (err) res.redirect("/blogs");
+    else res.redirect("/blogs/" + req.params.id);
+  });
+})
+
+// DESTROY ROUTE
+app.delete("/blogs/:id", function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) res.redirect("/blogs");
+    else res.redirect("/blogs");
+  })
+})
 
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log("RESTfulBlogApp Server has started");
